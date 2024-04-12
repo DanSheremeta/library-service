e76697bf81b6c92a630c48a8a10334613d301020
DanSheremeta <danylo3002@gmail.com>
2024-04-12 23:45:18 +0300

    Implemented permission IsAdminOrReadOnly, add permission IsAdminOrReadOnly to BookViewSet and add JWT auth for whole project

--- book/views.py
+++ book/views.py
@@ -2,8 +2,10 @@ from rest_framework.viewsets import ModelViewSet
 
 from book.models import Book
 from book.serializers import BookSerializer
+from book.permissions import IsAdminUserOrReadOnly
 
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAdminUserOrReadOnly,)
