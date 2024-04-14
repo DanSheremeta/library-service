4becb779416bc42756070c11b5357ecede139bcc
DanSheremeta <danylo3002@gmail.com>
2024-04-14 15:46:01 +0300

    Implemented borrowing create endpoint. Created serializer with validation for book.inventory.

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from book.models import Book
 from borrowing.models import Borrowing
 from book.serializers import BookSerializer
 
@@ -19,3 +20,19 @@ class BorrowingSerializer(serializers.ModelSerializer):
 
 class BorrowingListSerializer(BorrowingSerializer):
     book = BookSerializer(many=False, read_only=True)
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "book",
+        )
+
+    def validate_book(self, book):
+        if book.inventory == 0:
+            raise serializers.ValidationError("There is no available book to borrow")
+        return book
--- borrowing/views.py
+++ borrowing/views.py
@@ -4,13 +4,14 @@ from rest_framework.viewsets import GenericViewSet
 from borrowing.models import Borrowing
 from borrowing.serializers import (
     BorrowingSerializer,
-    BorrowingListSerializer,
+    BorrowingListSerializer, BorrowingCreateSerializer,
 )
 
 
 class BorrowingViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
     GenericViewSet,
 ):
     queryset = Borrowing.objects.all()
@@ -18,4 +19,12 @@ class BorrowingViewSet(
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
             return BorrowingListSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
         return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        book = serializer.validated_data["book"]
+        book.inventory -= 1
+        book.save()
+        serializer.save(user=self.request.user)
