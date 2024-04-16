c711c2cd335b9933742841f57e8554a7beb57e24
DanSheremeta <danylo3002@gmail.com>
2024-04-16 13:05:57 +0300

    Implemented return Borrowing functionality, borrowing can not be returned twice, after return add 1 book to inventory.

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,11 +1,14 @@
-from rest_framework import mixins
+from rest_framework import mixins, status
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from borrowing.models import Borrowing
 from borrowing.serializers import (
     BorrowingSerializer,
-    BorrowingListSerializer, BorrowingCreateSerializer,
+    BorrowingListSerializer, BorrowingCreateSerializer, BorrowingReturnSerializer,
 )
 
 
@@ -23,6 +26,8 @@ class BorrowingViewSet(
             return BorrowingListSerializer
         if self.action == "create":
             return BorrowingCreateSerializer
+        if self.action == "borrowing_return":
+            return BorrowingReturnSerializer
         return BorrowingSerializer
 
     def get_queryset(self):
@@ -48,3 +53,35 @@ class BorrowingViewSet(
         book.inventory -= 1
         book.save()
         serializer.save(user=self.request.user)
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="return",
+    )
+    def borrowing_return(self, request, pk=None):
+        borrowing = self.get_object()
+
+        if borrowing.user != self.request.user:
+            return Response(
+                {"error": "You can not return someone else's borrowing"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if not borrowing.is_active:
+            return Response(
+                {"error": "You can not return inactive borrowing"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        book = borrowing.book
+        book.inventory += 1
+        book.save()
+
+        borrowing.is_active = False
+        borrowing.save()
+        message = {
+            "message": f"You successfully returned {book.title} book!"
+        }
+
+        return Response(message, status=status.HTTP_201_CREATED)
